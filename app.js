d826f72aa2a13a1c2aff7d10564779d79485aac7
Emma Marriott <mekmarriott@gmail.com>
2015-03-07 17:39:42 -0800

    added patient glucose observations in console

--- app.js
+++ app.js
@@ -33,32 +33,17 @@ app.get('/', function(req, res){
 	res.render('home.ejs');
 });
 
-// VCAP_SERVICES contains all the credentials of services bound to
-// this application. For details of its content, please refer to
-// the document or sample of each service.
-// var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-//service name, check the VCAP_SERVICES in Bluemix to get the name of the services you have
-  // var service_name = 'tradeoff_analytics';
- 
-  // if (services[service_name]) {
-  //   var svc = services[service_name][0].credentials;
-  //   service_url = svc.url;
-  //   service_username = svc.username;
-  //   service_password = svc.password;
-  // } else {
-  //   console.log('The service '+ service_name +' is not in the VCAP_SERVICES. Did you forget to bind it?');
-  // }
 
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 // The port on the DEA for communication with the application:
 var port = (process.env.VCAP_APP_PORT || 3000);
 // Start server
-server = http.createServer(app).listen(port, function () {
-	console.log('App started on port ' + port);
-});
+// server = http.createServer(app).listen(port, function () {
+// 	console.log('App started on port ' + port);
+// });
 
-socket.listen(server);
-// app.listen(port, host);
-// console.log('App started on port ' + port);
+// socket.listen(server);
+app.listen(port, host);
+console.log('App started on port ' + port);
 
